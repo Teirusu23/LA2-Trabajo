6ebb3ca45c18cb76642c175b3ea658b9eb002553
Teirusu23 <89674254+Teirusu23@users.noreply.github.com>
2026-07-03 22:06:21 -0600

    Ejercicio11Streamlit

---
+++ Competencia 2/Ejercicio11Streamlit/ExprParser.py
@@ -0,0 +1,156 @@
+# Generated from Expr.g4 by ANTLR 4.13.2
+# encoding: utf-8
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+	from typing import TextIO
+else:
+	from typing.io import TextIO
+
+def serializedATN():
+    return [
+        4,1,20,10,2,0,7,0,2,1,7,1,1,0,1,0,1,0,1,1,1,1,1,1,0,0,2,0,2,0,1,
+        1,0,18,19,7,0,4,1,0,0,0,2,7,1,0,0,0,4,5,3,2,1,0,5,6,5,0,0,1,6,1,
+        1,0,0,0,7,8,7,0,0,0,8,3,1,0,0,0,0
+    ]
+
+class ExprParser ( Parser ):
+
+    grammarFileName = "Expr.g4"
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    sharedContextCache = PredictionContextCache()
+
+    literalNames = [ "<INVALID>", "'public'", "'class'", "'{'", "'static'", 
+                     "'void'", "'('", "'String'", "'['", "']'", "'int'", 
+                     "'='", "'+'", "'.'", "')'", "';'", "'}'" ]
+
+    symbolicNames = [ "<INVALID>", "PUBLIC", "CLASS", "LLAVEA", "STATIC", 
+                      "VOID", "ParentA", "STRING", "CorchA", "CorchC", "INT", 
+                      "Igual", "MAYOR", "Punto", "ParentC", "PuntoComa", 
+                      "LLAVEC", "CADENA", "NUM", "IDENTIFICADOR", "WS" ]
+
+    RULE_root = 0
+    RULE_expr = 1
+
+    ruleNames =  [ "root", "expr" ]
+
+    EOF = Token.EOF
+    PUBLIC=1
+    CLASS=2
+    LLAVEA=3
+    STATIC=4
+    VOID=5
+    ParentA=6
+    STRING=7
+    CorchA=8
+    CorchC=9
+    INT=10
+    Igual=11
+    MAYOR=12
+    Punto=13
+    ParentC=14
+    PuntoComa=15
+    LLAVEC=16
+    CADENA=17
+    NUM=18
+    IDENTIFICADOR=19
+    WS=20
+
+    def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.13.2")
+        self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
+        self._predicates = None
+
+
+
+
+    class RootContext(ParserRuleContext):
+        __slots__ = 'parser'
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+        def expr(self):
+            return self.getTypedRuleContext(ExprParser.ExprContext,0)
+
+
+        def EOF(self):
+            return self.getToken(ExprParser.EOF, 0)
+
+        def getRuleIndex(self):
+            return ExprParser.RULE_root
+
+
+
+
+    def root(self):
+
+        localctx = ExprParser.RootContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 0, self.RULE_root)
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 4
+            self.expr()
+            self.state = 5
+            self.match(ExprParser.EOF)
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+    class ExprContext(ParserRuleContext):
+        __slots__ = 'parser'
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+        def NUM(self):
+            return self.getToken(ExprParser.NUM, 0)
+
+        def IDENTIFICADOR(self):
+            return self.getToken(ExprParser.IDENTIFICADOR, 0)
+
+        def getRuleIndex(self):
+            return ExprParser.RULE_expr
+
+
+
+
+    def expr(self):
+
+        localctx = ExprParser.ExprContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 2, self.RULE_expr)
+        self._la = 0 # Token type
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 7
+            _la = self._input.LA(1)
+            if not(_la==18 or _la==19):
+                self._errHandler.recoverInline(self)
+            else:
+                self._errHandler.reportMatch(self)
+                self.consume()
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+
+
+
